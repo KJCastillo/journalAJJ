cd4e5a04c8a9f1140d82148f2055601e8b793c49
Kevin Castillo <kevincastillo318@gmail.com>
2023-01-15 14:13:25 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Create from "./pages/Create";
+import Signup from "./pages/Signup";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/create" element={<Create />}/>
+          <Route path="/signup" element={<Signup />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
