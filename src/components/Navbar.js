84bc3f73cb70085dee36f1e391c6e7267d059ad8
Kevin Castillo <kevincastillo318@gmail.com>
2023-01-16 15:50:09 -0800

    navbar building out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
 import "./Navbar.css"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <ul>
+            <li className="logo">
+                <img src="" alt="" />
+                <span>Journal AJJ</span></li>
+            <li> <Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><button className="btn">Logout</button></li>
+        </ul>
+    </div>
   )
 }
