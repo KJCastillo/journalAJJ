0d53cf53f3c0de455dcfa9ee3f64df1b277f9df9
Kevin Castillo <kevincastillo318@gmail.com>
2023-01-17 17:02:15 -0800

    login works

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,23 +1,23 @@
-// import { useState } from "react";
-// import { auth } from "../firebase/config";
-// import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
-// export default function useLogout() {
-//   const [error, setError] = useState(null);
-//   const [isPending, setIsPending] = useState(false);
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 
-//   const logout = (email, password) => {
-//     setError(null);
+  const login = (email, password) => {
+    setError(null);
 
-//     signInWithEmailAndPassword(auth, email, password)
-//       .then((res) => {
-//         console.log("loggedout", res.user);
-//       })
-//       .catch((err) => {
-//         setError(err.message);
-//         setIsPending(false);
-//       });
-//   };
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log("logged in", res.user);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  };
 
-//   return { error, isPending, logout };
-// }
+  return { error, isPending, login };
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,46 @@
-import "./Login.css"
+import { useState } from "react";
+import "./Login.css";
+import { useLogin } from "../hooks/useLogin"
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useLogin()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    login(email, password)
+  }
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+        <label>
+          <span>
+            Email:
+            <input
+              required
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </span>
+        </label>
+        <label>
+          <span>
+            Password:
+            <input
+              required
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </span>
+        </label>
+        <button className="btn">Log In</button>
+      </form>
+    </div>
+  );
 }
