eed3429399db246bd51c9cfe0003ac5a804f18cf
Kevin Castillo <kevincastillo318@gmail.com>
2023-01-29 10:37:54 -0800

    collection hook error check added

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import "./Dashboard.css"
 import { JournalList } from '../pages/journals/JournalList'
 // import { JournalForm } from '../pages/journals/JournalForm'
 import { useCollection } from "../hooks/useCollection"
+import "./Dashboard.css"
 
 export default function Dashboard() {
   const { document: journal} = useCollection('moves')  
--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -4,19 +4,29 @@ import { collection, onSnapshot } from "firebase/firestore";
 
 export const useCollection = (c) => {
   const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     let ref = collection(db, c);
 
-    const unsub = onSnapshot(ref, (snapshot) => {
-      let results = [];
-      snapshot.docs.forEach((doc) => {
-        results.push({ id: doc.id, ...doc.data() });
-      });
-      setDocument(results);
-    });
-    return () => unsub()
+    const unsub = onSnapshot(
+      ref,
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ id: doc.id, ...doc.data() });
+        });
+        setDocument(results);
+
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError("could not fetch the data");
+      }
+    );
+    return () => unsub();
   }, [c]);
 
-  return {document}
+  return { document, error };
 };
