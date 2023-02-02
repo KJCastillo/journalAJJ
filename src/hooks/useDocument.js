85b85d43e86be942a3150e3309d750a21048428a
Kevin Castillo <kevincastillo318@gmail.com>
2023-02-02 08:12:13 -0800

    fixing useDocument hook

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";    
 import { db } from "../firebase/config";
+import { collection, onSnapshot } from "firebase/firestore";
 
 export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null);  
 
   //realtime data for document
   useEffect(() => {
-    const ref = db.collection(collection).doc(id);
+    const ref = collection(db)
+    //collection(db, collection) .doc(id);
+    
 
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
--- src/pages/journals/Journal.js
+++ src/pages/journals/Journal.js
@@ -1,24 +1,24 @@
-// import { useParams } from "react-router-dom";
-// import { useDocument } from "../../hooks/useDocument";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
 import "./Journal.css";
 // import ProjectComments from "./ProjectComments";
 // import ProjectSummary from "./ProjectSummary";
 
 export default function Project() {
-//   const { id } = useParams();
-//   const { document, error } = useDocument("projects", id);
+  const { id } = useParams();
+  const { document, error } = useDocument("technique", id);
 
-//   if (error) {
-//     return <div className="error">{error}</div>;
-//   }
-
-//   if (!document) {
-//     return <div className="loading">Loading...</div>;
-//   }
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
 
   return (
     <div className="project-details">
-        <p>test test test</p>
+      <p>test test test</p>
+      <h1>{document.title}</h1>
       {/* <ProjectSummary project={document}/>
       <ProjectComments project={document}/> */}
     </div>
