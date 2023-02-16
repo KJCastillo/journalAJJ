7f9818287a83f2c2666b7c8e0de0a49cddc1bfdc
Kevin Castillo <kevincastillo318@gmail.com>
2023-02-16 08:05:32 -0800

    working on user only entries

--- src/pages/journals/JournalList.js
+++ src/pages/journals/JournalList.js
@@ -1,19 +1,26 @@
 import "./JournalList.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function JournalList({ techniques }) {
+  const { user } = useAuthContext();
+
   return (
     <div className="journal-list">
       {techniques.length === 0 && <p>No Techniques Added</p>}
-      {techniques.map(technique => (
+
+      {/* {user.uid === techniques.createdBy.id && ()} */}
+
+      {techniques.map((technique) => (
         <Link to={`/journals/${technique.id}`} key={technique.id}>
           <h4>{technique.title}</h4>
           {/* <p>{technique.technique}</p> */}
-          <h5>{technique.position} - {technique.type} - {technique.style}</h5>
+          <h5>
+            {technique.position} - {technique.type} - {technique.style}
+          </h5>
           <p className="taught-by">{technique.coach}</p>
         </Link>
       ))}
     </div>
   );
 }
-
