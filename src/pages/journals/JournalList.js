337b3f860e9a5539880a898792fda4d96d6a8830
Kevin Castillo <kevincastillo318@gmail.com>
2023-02-19 13:58:08 -0800

    journal list cleaned up

--- src/pages/journals/JournalList.js
+++ src/pages/journals/JournalList.js
@@ -1,17 +1,10 @@
 import "./JournalList.css";
 import { Link } from "react-router-dom";
-//import { useAuthContext } from "../../hooks/useAuthContext";
 
-export default function JournalList({ techniques, uid }) {
- // const { user } = useAuthContext();
-
-
+export default function JournalList({ techniques }) {
   return (
     <div className="journal-list">
       {techniques.length === 0 && <p>No Techniques Added</p>}
-
-      {/* {user.uid === techniques.createdBy.id && ()} */}
-
       {techniques.map((technique) => (
         <Link to={`/journals/${technique.id}`} key={technique.id}>
           <h4>{technique.title}</h4>
